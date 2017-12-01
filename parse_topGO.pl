5f9d2c24f2dcb700f019e193c955056907c7df6c
Marcelo <brandao.marcelo@gmail.com>
2017-12-01 14:27:30 -0200

    topGO parse tweaked

--- parse_topGO.pl
+++ parse_topGO.pl
@@ -2,19 +2,29 @@
 
 use strict;
 use warnings;
+use Getopt::Long;
 
-my %stuff; 
+my ($infile);
+    GetOptions ('infile=s' => \$infile);
+        if ((!$infile)) {
+    print "Some required arguments are missing.\nYou must use this as follow:\n$0 --infile [Annotation file]\n";
+    exit 1
+}
+#based on https://unix.stackexchange.com/questions/264058/merge-multiple-lines-in-same-file-based-on-column-1
 
-my ( $id, @header ) = split '\t', <>;
+my %anDATA; 
+open(DATA, $infile) or die "Couldn't open file file.txt, $!";
 
-while ( <> ) { 
+my ( $id, @anLINE ) = split '\t', <DATA>;
+splice @anLINE, 0, 3;
+while ( <DATA> ) { 
    my ( $key, @values ) = split; 
    my %row;
-   @row{@header} = @values; 
-   push ( @{$stuff{$key}{$_}}, $row{$_} ) for keys %row;
+   @row{@anLINE} = @values; 
+   push ( @{$anDATA{$key}{$_}}, $row{$_} ) for keys %row;
 }
 
-print join ( "\t", $id, @header),"\n";
-foreach my $key ( sort keys %stuff ) {
-   print join ("\t", $key, map { join ";", @{$stuff{$key}{$_}}} @header), "\n";
+# print join ( "\t", $id, @anLINE),"\n"; # first row as header
+foreach my $key ( sort keys %anDATA ) {
+   print join ("\t", $key, map { join ",", @{$anDATA{$key}{$_}}} @anLINE), "\n";
 }

499edec824fb5c588b2b32eaf254ae8bb2fb40cc
Marcelo Brandao <brandaom@unicamp.br>
2018-02-02 14:56:06 -0200

    Fix update files, add gi2uniprot

--- Update/update_DeNSAS.sh
+++ Update/update_DeNSAS.sh
@@ -39,44 +39,53 @@ gunzip go_monthly-assocdb-data.gz
 #Association
 #############
 
-mysql -uannotate -pb10ine0! -h localhost annotate < association.sql &
-# mysqlimport -uannotate -pb10ine0! -h localhost --use-threads=60 --local annotate association.txt
+mysql -uannotate -pb10ine0! -h 143.106.4.169 annotate < association.sql &
+# mysqlimport -uannotate -pb10ine0! -h 143.106.4.169 --use-threads=10 --local annotate association.txt
 
 ########
 #dbxref
 ########
 
-mysql -uannotate -pb10ine0! -h localhost annotate < dbxref.sql &
-# mysqlimport -uannotate -pb10ine0! -h localhost --use-threads=10 --local annotate $ASSDBDIR/dbxref.txt
+mysql -uannotate -pb10ine0! -h 143.106.4.169 annotate < dbxref.sql &
+# mysqlimport -uannotate -pb10ine0! -h 143.106.4.169 --use-threads=10 --local annotate $ASSDBDIR/dbxref.txt
 
 #########
 #evidence
 #########
 
-mysql -uannotate -pb10ine0! -h localhost annotate < evidence.sql &
-# mysqlimport -uannotate -pb10ine0! -h localhost --use-threads=60 --local annotate $ASSDBDIR/evidence.txt
+mysql -uannotate -pb10ine0! -h 143.106.4.169 annotate < evidence.sql &
+# mysqlimport -uannotate -pb10ine0! -h 143.106.4.169 --use-threads=10 --local annotate $ASSDBDIR/evidence.txt
 
 ##############
 #gene_product
 ##############
 
-mysql -uannotate -pb10ine0! -h localhost annotate < gene_product.sql &
-# mysqlimport -uannotate -pb10ine0! -h localhost --use-threads=10 --local annotate $ASSDBDIR/gene_product.txt
+mysql -uannotate -pb10ine0! -h 143.106.4.169 annotate < gene_product.sql &
+# mysqlimport -uannotate -pb10ine0! -h 143.106.4.169 --use-threads=10 --local annotate $ASSDBDIR/gene_product.txt
 
 #########
 #species
 #########
 
-mysql -uannotate -pb10ine0! -h localhost annotate < species.sql &
-# mysqlimport -uannotate -pb10ine0! -h localhost --use-threads=10 --local annotate $ASSDBDIR/species.txt
+mysql -uannotate -pb10ine0! -h 143.106.4.169 annotate < species.sql &
+# mysqlimport -uannotate -pb10ine0! -h 143.106.4.169 --use-threads=10 --local annotate $ASSDBDIR/species.txt
 
 #######
 #term
 #######
 
-mysql -uannotate -pb10ine0! -h localhost annotate < term.sql &
-# mysqlimport -uannotate -pb10ine0! -h localhost --use-threads=10 --local annotate $ASSDBDIR/term.txt
+mysql -uannotate -pb10ine0! -h 143.106.4.169 annotate < term.sql &
+# mysqlimport -uannotate -pb10ine0! -h 143.106.4.169 --use-threads=10 --local annotate $ASSDBDIR/term.txt
+
+############
+#graph_path
+############
+
+mysql -uannotate -pb10ine0! -h 143.106.4.169 annotate < graph_path.sql &
+
 wait
+
+
 
 ################
 #gene2accession
@@ -84,18 +93,65 @@ wait
 
 cd $DOWNLDATA
 gunzip gene2accession.gz
-mysqlimport -uannotate -pb10ine0! -h localhost --use-threads=60 --local --delete -d annotate $DOWNLDATA/gene2accession
+mysqlimport -uannotate -pb10ine0! -h 143.106.4.169 --use-threads=10 --local --ignore-lines=1 --columns "tax_id,GeneID,status,RNA_nucleotide_accession.version,RNA_nucleotide_gi,protein_accession.version,protein_gi,genomic_nucleotide_accession.version,genomic_nucleotide_gi,start_position_on_the_genomic_accession,end_position_on_the_genomic_accession,orientation,assembly,mature_peptide_accession.version,mature_peptide_gi,Symbol" --delete -d densas $DOWNLDATA/gene2accession
 
 ##################
 #gene_association
 ##################
 
 zcat goa_uniprot_all.gaf.gz | cut -f 2,5,7,9 > gene_association.txt
-mysqlimport -uannotate -pb10ine0! -h localhost --use-threads=60 --local --delete -d annotate $DOWNLDATA/gene_association.txt
+mysqlimport -uannotate -pb10ine0! -h 143.106.4.169 --use-threads=10 --local --delete -d densas $DOWNLDATA/gene_association.txt
 
 ###########
 #gene_info
 ###########
 
 gunzip gene_info.gz
-mysqlimport -uannotate -pb10ine0! -h localhost --use-threads=60 --local --delete -d annotate $DOWNLDATA/gene_info
+mysqlimport -uannotate -pb10ine0! -h 143.106.4.169 --use-threads=10 --local --delete -d densas $DOWNLDATA/gene_info
+
+###############
+#MEROPS_DOMAIN
+###############
+
+mv domain.txt MEROPS_domain.txt
+mysqlimport -uannotate -pb10ine0! -h 143.106.4.169 --use-threads=10 --local --delete -d densas $DOWNLDATA/MEROPS_domain.txt
+
+###########
+#MEROPS2GO
+###########
+
+mv GO_annotation.txt MEROPS2GO.txt
+mysqlimport -uannotate -pb10ine0! -h 143.106.4.169 --use-threads=10 --local --delete -d densas $DOWNLDATA/MEROPS2GO.txt
+
+#########
+#GO_term
+#########
+
+mysql -uannotate -pb10ine0! -h bioinfo.cbmeg.unicamp.br densas -e "SELECT DISTINCT
+  term.acc AS go_id,
+  term.name AS term,
+  CASE WHEN term.term_type = 'biological_process' THEN 'P' WHEN term.term_type = 'molecular_function' THEN 'F' WHEN term.term_type = 'cellular_component' THEN 'C' END AS category,
+  p.distance
+FROM term
+  INNER JOIN graph_path p
+    ON p.term2_id = term.id
+  INNER JOIN term root
+    ON p.term1_id = root.id
+WHERE root.is_root = 1
+AND term.is_obsolete <> 1;" > $DOWNLDATA/GO_term.txt
+
+mysqlimport -uannotate -pb10ine0! -h 143.106.4.169 --use-threads=10 --local --delete -d densas $DOWNLDATA/GO_term.txt
+
+######
+#RFAM
+######
+
+mv family.txt.gz RFAM.txt.gz
+gunzip RFAM.txt.gz
+mysqlimport -uannotate -pb10ine0! -h 143.106.4.169 --use-threads=10 --local --delete -d densas $DOWNLDATA/RFAM.txt
+
+#########
+#RFAM2GO
+#########
+
+mysqlimport -uannotate -pb10ine0! -h 143.106.4.169 --use-threads=10 --local --delete --columns "rfam_id,GO_ID" -d densas $DOWNLDATA/RFAM2GO.txt

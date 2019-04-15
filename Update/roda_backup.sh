9aa825e367141293c5b80eb7c5b610d299eb0779
The Bioinfo guy <labis@unicamp.br>
2019-04-15 17:27:24 -0300

    Speedup Backup

--- Update/roda_backup.sh
+++ Update/roda_backup.sh
@@ -3,12 +3,10 @@
 #Set all variables
 date_bckp=`date +"%d-%m-%Y"`
 DIR="/home/mmbrand/auto_bckp/"
-SITEDIR="/home/ab3c1/public_html/"
 DBSERVER="143.106.4.87"
 DBNAME="densas"
-DBUSER="densas"
-DBPASS="b10ine0!"
-
+DBUSER="annotate"
+DBPASS=b10ine0!
 
 data_tables=(`mysql --user=$DBUSER -p$DBPASS -h$DBSERVER -e "SHOW TABLES;" $DBNAME | grep -Ev "(Database|information_schema|performance_schema|Tables_in_)"`)
 
@@ -19,16 +17,16 @@ data_tables=(`mysql --user=$DBUSER -p$DBPASS -h$DBSERVER -e "SHOW TABLES;" $DBNA
 
 
 if [ ! -d "${DIR}/$DBNAME" ]; then
-echo "Cool, a new database! Creating directory"
+echo $(date -u) "Cool, a new database! Creating directory"
 mkdir ${DIR}/$DBNAME
 fi
 
 if [ ! -d "${DIR}/$DBNAME/${date_bckp}" ]; then
-echo "Creating backup directory"
+echo $(date -u)  "Creating backup directory"
 mkdir ${DIR}/$DBNAME/${date_bckp}
 fi
 
-echo "  starting the backup database $DBNAME"
+echo $(date -u)  "starting the backup database $DBNAME"
 
 cd ${DIR}/$DBNAME/${date_bckp}
 for i in "${data_tables[@]}"
@@ -36,6 +34,10 @@ do
    :
    echo Backing up $i
    mysqldump -u$DBUSER -p$DBPASS -h$DBSERVER --add-drop-table $DBNAME $i > ${i}_${date_bckp}_$DBNAME.sql
-   gzip ${i}_${date_bckp}_$DBNAME.sql
+   gzip ${i}_${date_bckp}_$DBNAME.sql &
    # do whatever on $i
 done
+echo $(date -u) "Compressing files"
+wait
+#Delete older files
+find ${DIR}/$DBNAME/* -type d -ctime +180 | xargs ls -alh
